4e8ba366c5496f146222c3a37383921789aa539a
ruchi5134 <kn043613@gmail.com>
2025-02-25 21:13:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/TeamEspro/EsproMusicBot",
+    "UPSTRE_REPO"https://github.com/ruchi5134/Ruchi123
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
