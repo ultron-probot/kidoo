024964d45d1b48bd4b3b492a5c496f9e3a268c11
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-05-24 19:46:08 +0530

    Update Youtube.py

--- EsproMusic/platforms/Youtube.py
+++ EsproMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from EsproMusic.utils.database import is_on_off
+from EsproMusic.utils.formatters import time_to_seconds
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
         cmd,
