4252dc23e817f82b8bd521064c7214fa95cd7ac4
ultron-probot <smitjhn392@gmail.com>
2025-09-28 01:03:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -103,6 +103,8 @@ SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
+ANKT = ["💞", "🦋", "🔍", "🧪", "⚡️", "🔥", "🎩", "🌈", "🍷", "🥂", "🥃", "🕊️", "🪄", "🧨", "🦄"]
+
 
 def time_to_seconds(time):
     stringt = str(time)
