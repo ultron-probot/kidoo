a43e6f555fd105ef4be22d632da5efe3538804bf
ultron-probot <smitjhn392@gmail.com>
2025-10-20 10:55:51 +0530

    Update __init__.py

--- EsproMusic/plugins/__init__.py
+++ EsproMusic/plugins/__init__.py
@@ -1,19 +1,18 @@
 import glob
-from os.path import dirname, isfile
-
+import os
 
 def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+    work_dir = os.path.dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/**/*.py", recursive=True)
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
+    all_modules = []
+    for f in mod_paths:
+        if os.path.isfile(f) and f.endswith(".py") and not f.endswith("__init__.py"):
+            relative_path = os.path.relpath(f, work_dir)
+            module_name = relative_path.replace(os.sep, ".")[:-3]
+            all_modules.append(module_name)
 
     return all_modules
-
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
