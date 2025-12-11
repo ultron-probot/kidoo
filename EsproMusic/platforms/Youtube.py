0146b870f2f456c7d805445b95bbb6418ee52ce6
ultron-probot <smitjhn392@gmail.com>
2025-12-11 21:58:19 +0530

    Update Youtube.py

--- EsproMusic/platforms/Youtube.py
+++ EsproMusic/platforms/Youtube.py
@@ -16,7 +16,7 @@ FALLBACK_API_URL = "https://shrutibots.site"
 
 async def load_api_url():
     global YOUR_API_URL
-    logger = LOGGER("SONALI.platforms.Youtube.py")
+    logger = LOGGER("EsproMusic.platforms.Youtube.py")
     
     try:
         async with aiohttp.ClientSession() as session:
