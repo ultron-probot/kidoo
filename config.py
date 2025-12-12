edd042b4c8a98f0a56ef3fa7bbe1a5c78a72797f
ultron-probot <smitjhn392@gmail.com>
2025-12-12 11:38:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 # API URL - here
-API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
 API_KEY = getenv("30DxNexGenBots71dc22") # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
