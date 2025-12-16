50c6b1dfec1dd7d334b3415de554b42b2d90046a
ultron-probot <smitjhn392@gmail.com>
2025-12-16 21:28:45 +0530

    Update and rename Youtube (6).py to Youtube.py

--- EsproMusic/platforms/Youtube.py
+++ EsproMusic/platforms/Youtube.py
@@ -7,7 +7,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpytho import VideosSearch
 from EsproMusic.utils.database import is_on_off
 from EsproMusic.utils.formatters import time_to_seconds
 import os
@@ -596,4 +596,5 @@ class YouTubeAPI:
             direct = True
             downloaded_file = await download_song(link)
         return downloaded_file, direct
+
 
